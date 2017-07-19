7fc30a21fbdb9a30ef5a2ad375c13eeae43c320d
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-07-19 20:28:00 +0200

    create convex hull of pareto points and prep plot data

--- server/pareto.R
+++ server/pareto.R
@@ -2,6 +2,7 @@
 library(geometry)
 library(rPref)
 library(plyr)
+library(ggplot2)
 
 # filters a dataset to only include the pareto points
 pareto.points = function(data) {
@@ -23,5 +24,65 @@ delaunay.edges = function(ppts) {
   print("edges done")
   res = data.frame(edges)
   names(res) = c("p1", "p2")
-  res
+  unique(res)
 }
+
+# convex hull w/ plane intersection
+segment.intersects = function(fp, d1, d2, pt1, pt2) {
+  # since everything on the d1xd2 plane conatains all numbers we 
+  # can just cheat with 0
+  fp.p = unlist(fp)
+  pt1.p = unlist(pt1)
+  pt2.p = unlist(pt2)
+  fp.p[c(d1,d2)] = 0
+  pt1.p[c(d1,d2)] = 0
+  pt2.p[c(d1,d2)] = 0
+  if(all(pt1.p == pt2.p)) return(NULL)
+  
+  t = (fp.p-pt1.p) / (pt2.p-pt1.p)
+  t = t[is.finite(t)] # Infs and NaNs aren't important
+  
+  # only inside the segment between 0 and 1
+  if(!is.null(t[1]) & all(t==t[1]) & t[1] >= 0 & t[1] <= 1) {
+    pt1 + t[1] * (pt2-pt1)
+  } else {
+    NULL
+  }
+}
+
+intersect.pts = function(data, edges, fp, d1, d2) {
+  f = function(r) as.data.frame(segment.intersects(fp, d1, d2, data[r$p1,], data[r$p2,]))
+  res = adply(edges, 1, f)
+  if(nrow(res)>0) {
+    res = res[,names(data)]
+    res[!is.na(res[,1]),]
+  } else {
+    # need a proper empty data frame
+    res = data.frame(matrix(nrow=0,ncol=length(names(data))))
+    names(res) = names(data)
+    res
+  }
+}
+
+# plotting
+gen.plot.data = function(data, edges, n) {
+  d = ncol(data)
+  samples = data.frame(matrix(runif(d*n), ncol=d))
+  dims = t(combn(d,2))
+  adply(1:n, 1, function(rid) {
+    fp = samples[rid,]
+    res = adply(dims, 1, function(d) {
+      res2 = intersect.pts(data, edges, fp, d[1], d[2])
+      if(nrow(res2)>0) {
+        res2$d1 = d[1]
+        res2$d2 = d[2]
+      }
+      res2
+    })
+    if(nrow(res) > 0) {
+      res$fpid = rid
+    }
+    res
+  })
+}
+
