3a9c1978d3bb9667a553177c0a4a0b3ce0f4d80e
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-08-16 17:09:47 +0200

    basic tests for simplexes works!

---
+++ test_simplex_intersections.R
@@ -0,0 +1,44 @@
+debugSource('server/pareto.R')
+
+simple.simplex = matrix(c(
+  2,2,1,
+  2,1,2,
+  1,1,0,
+  3,1,0), 
+  ncol=3, byrow=TRUE)
+
+planar.triangle = matrix(c(
+  1,1,1,
+  3,1,1,
+  2,2,1),
+  ncol=3, byrow=TRUE)
+
+test_that("tet with 2D plane", {
+  res <- simplex.intersect.test(1, 3, c(0.0, 1.5, 0.0), simple.simplex)
+  # expect_equal(res, data.frame(d1.min=c(NA, 1.5, 2.0, 1.5), # columns
+  #                              d2.min=c(NA, 0.5, 1.5, 0.5),
+  #                              d1.max=c(NA, 2.5, 2.5, 2.0),
+  #                              d2.max=c(NA, 0.5, 0.5, 1.5)))
+  expect_equal(as.vector(res$d1.min), c(NA, 1.5, 2.0, 1.5))
+  expect_equal(as.vector(res$d2.min), c(NA, 0.5, 1.5, 0.5))
+  expect_equal(as.vector(res$d1.max), c(NA, 2.5, 2.5, 2.0))
+  expect_equal(as.vector(res$d2.max), c(NA, 0.5, 0.5, 1.5))
+})
+
+test_that("3d-embedded triangle in plane (intersection)", {
+  res <- simplex.intersect.test(1, 2, c(1.0, 1.0, 1.0), planar.triangle)
+  
+  expect_that(nrow(res), equals(3))
+
+  expect_equal(as.vector(res$d1.min), c(2,1,1))
+  expect_equal(as.vector(res$d1.max), c(3,2,3))
+  expect_equal(as.vector(res$d2.min), c(2,1,1))
+  expect_equal(as.vector(res$d2.max), c(1,2,1))
+})
+
+test_that("3d-embedded triangle in plane (non-intersection)", {
+  res <- simplex.intersect.test(1, 2, c(1.0, 1.0, 0.0), planar.triangle)
+  
+  expect_that(nrow(res), equals(3))
+  expect_equal(as.vector(unlist(res)), rep(NA, 3*4))
+})
