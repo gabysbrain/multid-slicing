efb657608b7554c246bef591773c077e687f9ce7
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-08-16 17:08:37 +0200

    need to also check for co-planarness

--- server/pareto.R
+++ server/pareto.R
@@ -6,6 +6,7 @@ library(dplyr)
 library(ggplot2)
 library(gridExtra)
 library(MASS)
+library(Matrix)
 
 # filters a dataset to only include the pareto points
 pareto.points = function(data) {
@@ -137,8 +138,8 @@ plot.hull.discrete = function(ppts, n=10) {
   plots = list()
   for(i in 1:(d-1)) { # d1 varies the slowest
     for(j in (i+1):d) {
-      pd = plot.data %>% filter(d1==i&d2==j)
-      pd = pd[order(pd[,"x1"]),]
+      pd = plot.data %>% filter(d1==i&d2==j) %>% unique()
+      pd = pd[order(pd[,"x2"]),]
       p = ggplot(pd, aes(x=x1,y=x2,group=fpid)) +
             geom_point() + 
             geom_line(aes(colour=fpid)) +
@@ -154,22 +155,19 @@ plot.hull.discrete = function(ppts, n=10) {
 
 simplex.intersect.test = function(d1, d2, focus.pt, simplex) {
   n = nrow(simplex) # number of lambdas, dimensionality of the simplex+1
-  r.last = simplex[n,]
-  T = t(sub.rows(simplex, r.last))[,-n]
+  T = rbind(t(simplex), rep(1,n))
   T.inv = ginv(T) # T isn't always square
   
   # compute lambda as best we can (there will be 3 parts)
-  rr = focus.pt
+  rr = c(unlist(focus.pt), 1)
   rr[c(d1,d2)] = 0
-  rr = rr - r.last
-  rr.x = array(0,length(focus.pt))
-  rr.y = array(0,length(focus.pt))
+  rr.x = array(0,length(rr))
+  rr.y = array(0,length(rr))
   rr.x[d1] = rr.y[d2] = 1
   # we are trying to compute T^(-1) . [x-xn,y-yn,...,z-zn]
-  lambda.rest = T.inv %*% t(rr) # column vector multiplication
+  lambda.c = as.vector(T.inv %*% rr) # column vector multiplication
   lambda.x = as.vector(T.inv %*% rr.x)
   lambda.y = as.vector(T.inv %*% rr.y)
-  lambda.c = as.vector(lambda.rest)
   
   # find the d1 and d2 ranges that make each lambda 0
   intersect.range = data.frame(d1.min=array(NA,n),d1.max=array(NA,n),
@@ -177,96 +175,49 @@ simplex.intersect.test = function(d1, d2, focus.pt, simplex) {
   
   # most indices are based on solving ax + by + c = 0
   # but keeping the other lambdas between 0 and 1
-  for(i in 1:(n-1)) {
-    rng = NULL
+  for(i in 1:n) {
     # put y=mx+b into each other lambda formula and try and get a good range
-    for(j in (1:n)[-i]) { # don't intersect i with itself
-      # compute the range
-      new.rng = NULL
-      # figure out the bounds of variable d1 that will keep b_j between 0 and 1
-      if(j != n) {
-        new.rng = common.cross.range(lambda.x, lambda.y, lambda.c, i, j)
-      } else {
-        new.rng = lastn.cross.range(lambda.x, lambda.y, lambda.c, i)
-      }
-      if(!is.null(rng)) # special first case
-        new.rng = intersect.ranges(new.rng,rng)
-      rng = new.rng
-    }
+    ranges = common.cross.range(lambda.x, lambda.y, lambda.c, i)
+    rng = c(max(ranges[,1]), min(ranges[,2]))
+    if(rng[2]<rng[1]) rng = c(NA,NA)
+
     # problem is b_n = 1 - b_1 - ... - b_(n-1)
     # so this requires special work
     # if we got an intersection then we're good!
     if(all(!is.na(rng))) {
       y.rng = (-lambda.x[i] * rng - lambda.c[i])/lambda.y[i]
-      intersect.range[i,"d1.min"] = rng[1]
-      intersect.range[i,"d1.max"] = rng[2]
-      intersect.range[i,"d2.min"] = y.rng[1]
-      intersect.range[i,"d2.max"] = y.rng[2]
-    }
-  }
-  
-  # the last index (n) is based on solving 1 - sum_i (a_i x + b_i y + c) = 0
-  # but keeping the other lambdas in range
-  c.sum = sum(lambda.c)
-  x.sum = sum(lambda.x)
-  y.sum = sum(lambda.y)
-  if(x.sum!=0 && y.sum!=0) {
-    x.factor = -x.sum / y.sum
-    c.value = (1-c.sum) / y.sum # based on solving the final lambda for 0
-    rng = NULL
-    for(i in 1:(n-1)) {
-      cc = lambda.c[i] + lambda.y[i]*c.value
-      xx = lambda.x[i] - lambda.y[i]*x.factor
-      new.rng = sort(c(-cc/xx,(1-cc)/xx))
-      if(!is.null(rng)) {
-        new.rng = intersect.ranges(new.rng, rng)
+      # now we have to check if everything is co-planar
+      if(is.coplanar(simplex, rng, y.rng, focus.pt, d1, d2)) {
+        intersect.range[i,"d1.min"] = rng[1]
+        intersect.range[i,"d1.max"] = rng[2]
+        intersect.range[i,"d2.min"] = y.rng[1]
+        intersect.range[i,"d2.max"] = y.rng[2]
       }
-      rng = new.rng
     }
-    y.rng = x.factor * rng + c.value
-    intersect.range[n,"d1.min"] = rng[1]
-    intersect.range[n,"d1.max"] = rng[2]
-    intersect.range[n,"d2.min"] = y.rng[1]
-    intersect.range[n,"d2.max"] = y.rng[2]
   }
   
   intersect.range
 }
 
-common.cross.range = function(lambda.x, lambda.y, lambda.c, i, j) {
-  f1 = (lambda.y[j]*lambda.c[i] - lambda.y[i]*lambda.c[j]) /
-    (lambda.x[j]*lambda.y[i] - lambda.x[i]*lambda.y[j])
-  f2 = (lambda.y[i] + lambda.y[j]*lambda.c[i] - lambda.y[i]*lambda.c[j]) /
-    (lambda.x[j]*lambda.y[i] - lambda.x[i]*lambda.y[j])
-  sort(c(f1,f2))
+is.coplanar = function(simplex, x.rng, y.rng, focus.pt, d1, d2) {
+  # how many dimensions the simplex exists in
+  dims = nrow(simplex) - 1
+  pt1 = focus.pt
+  pt1[d1] = x.rng[1]
+  pt1[d2] = y.rng[1]
+  pt2 = focus.pt
+  pt2[d1] = x.rng[2]
+  pt2[d2] = y.rng[2]
+  mtx1 = as.matrix(adply(simplex, 1, function(r) r - pt1, .id=NULL))
+  mtx2 = as.matrix(adply(simplex, 1, function(r) r - pt2, .id=NULL))
+  rankMatrix(mtx1) == dims & rankMatrix(mtx2) == dims
 }
 
-lastn.cross.range = function(lambda.x, lambda.y, lambda.c, i) {
-  n = length(lambda.x)
-  x.sum = 0
-  c.sum = 0
-  for(k in (1:n)[-i]) {
-    x.sum = x.sum + lambda.x[k] - lambda.y[k]*lambda.x[i]/lambda.y[i]
-    c.sum = c.sum + lambda.c[k] - lambda.y[k]*lambda.c[i]/lambda.y[i]
-  }
-  c.sum = 1 - c.sum
-  f1 = -c.sum / x.sum
-  f2 = (1-c.sum) / x.sum
-  sort(c(f1,f2))
+common.cross.range = function(lambda.x, lambda.y, lambda.c, i) {
+  f1 = (lambda.c[i]*lambda.y - lambda.y[i]*lambda.c) /
+    (lambda.y[i]*lambda.x - lambda.x[i]*lambda.y)
+  f2 = (lambda.y[i] + lambda.c[i]*lambda.y - lambda.y[i]*lambda.c) /
+    (lambda.y[i]*lambda.x - lambda.x[i]*lambda.y)
+  matrix(apply(matrix(cbind(f1,f2)[-i,], ncol=2), 1, sort), ncol=2, byrow=TRUE)
 }
 
-# subtract a row vector from every row of a matrix
-sub.rows = function(mtx, v) {
-  adply(mtx, 1, function(r) r - v)
-}
-
-# return the intersection of 2 ranges or NA,NA if there's no intersection
-intersect.ranges = function(rng1,rng2) {
-  if(any(is.na(c(rng1,rng2))))
-    return(c(NA,NA))
-  rng = c(max(rng1[1],rng2[1]),min(rng1[2],rng2[2]))
-  # if the max and min reverse then there is no intersection
-  if(rng[2]<rng[1]) 
-    rng = c(NA,NA)
-  rng
-}
