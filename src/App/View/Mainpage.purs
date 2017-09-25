5264c8e5d3da5c524d8223d956221e7f40d0abbf
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-09-25 19:30:10 +0200

    dataset loader

--- src/App/View/Mainpage.purs
+++ src/App/View/Mainpage.purs
@@ -8,20 +8,35 @@ import App.State (State(..), DataInfo, FileLoadError(..))
 import App.View.ParetoSlices as PS
 import Data.Array as A
 import Data.Traversable (for_)
+import Data.Tuple (Tuple(..))
 import Pux.DOM.HTML (HTML)
 import Pux.DOM.Events (DOMEvent, onChange, onClick)
-import Text.Smolder.HTML (div, button, label, h2, input, span, ul, li, p, a)
-import Text.Smolder.HTML.Attributes (className, type', min, max, step, value)
+import Text.Smolder.HTML (div, label, h2, ul, li, p, a, select, option)
+import Text.Smolder.HTML.Attributes (className, value)
 import Text.Smolder.Markup ((!), (#!), text)
 import Loadable (Loadable(..))
 import Data.DataFrame as DF
 import Data.Int (toNumber)
 
+datasets = 
+  [ Tuple "sphere" "sphere_3d.json"
+  , Tuple "5D sphere" "sphere_5d.json"
+  , Tuple "Klein bottle" "klein.json"
+  ]
+
 view :: State -> HTML Event
 view (State st) =
   div do
-    button #! onClick DataFileChange ! value "sphere_3d.json" $ text "init"
+    viewOptions
     viewSlices st.dataset
+
+viewOptions :: HTML Event
+viewOptions = div ! className "view-options" $ do
+  label $ do
+    text "Dataset:"
+    select #! onChange DataFileChange $ do
+      for_ datasets $ \(Tuple lbl url) ->
+        option ! value url $ text lbl
 
 viewSlices :: forall d. Loadable FileLoadError (DataInfo d) -> HTML Event
 viewSlices Unloaded = div $ text "Nothing yet!"
