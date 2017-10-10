72c9beb1c9a05f30c284c0a50fdc1c39ab1f2d10
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-10-10 16:27:08 +0200

    use correct focus point

--- server/pareto.R
+++ server/pareto.R
@@ -194,7 +194,7 @@ simplex.intersect.test = function(d1, d2, focus.pt, simplex) {
   }
   T = matrix(unlist(T), ncol=ncol(T)) # need to force T to be a matrix
   T.inv = solve(T)
-  
+
   # compute lambda as best we can (there will be 3 parts)
   rr = c(unlist(focus.pt), 1)
   rr[c(d1,d2)] = 0
@@ -247,7 +247,7 @@ plot.intersect = function(tri.pts, focus.pt, d1, d2) {
   normal = array(1, 3)
   normal[c(d1,d2)] = 0
   plane.pts = c(normal, normal %*% -focus.pt)
-  simplex = rbind(as.matrix(tri.pts), t(fp))
+  simplex = rbind(as.matrix(tri.pts), t(focus.pt))
   base = matrix(c(0,0,0,0,1,0,1,1,0,1,0,0), ncol=3, byrow=TRUE)
   #base = base * 10
   #base = base - 5
@@ -257,7 +257,7 @@ plot.intersect = function(tri.pts, focus.pt, d1, d2) {
   tetramesh(matrix(1:4, nrow=1), simplex, clear=FALSE)
   #planes3d(a=0, b=0, c=1, d=-0.75+1e-5, col="green")
   planes3d(a=plane.pts[1], b=plane.pts[2], c=plane.pts[3], d=plane.pts[4]+1e-5, col="green")
-  points3d(fp[1], fp[2], fp[3], col="blue", size=10)
+  points3d(focus.pt[1], focus.pt[2], focus.pt[3], col="blue", size=10)
   axes3d(col="black")
   title3d(xlab="x", ylab="y", zlab="z")
  # bbox3d(col="grey", alpha=0.7)
