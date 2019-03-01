6bcd3c54994801a62db7a258247111351d38fe06
Thomas Torsney-Weir <torsneyt@gmail.com>
2019-03-01 10:38:47 +0100

    basic hyperslice test working now

--- slicing/tests/testthat/test-hyperslice.R
+++ slicing/tests/testthat/test-hyperslice.R
@@ -9,19 +9,6 @@ test.mesh.1 = list(
   simplices = rbind(c(1, 2, 3))
 )
 
-exp.slices.1 = list(
-  focusPoints = data.frame(x1=0.5, x2=0.5, x3=0.5),
-  simplices = data.frame(
-    fpid=c(1, 1, 1),
-    d1=c(1, 1, 2),
-    d2=c(2, 3, 3),
-    d1Min=c(0.5, 0, 0.0),
-    d2Min=c(0.5, 0, 0.5),
-    d1Max=c(0.0, 0, 0.5),
-    d2Max=c(0.5, 0, 0.5)
-  )
-)
-
 test_that("function checks for focus points", {
   expect_error(hyperslice(test.mesh.1), "either n or focus.points must be specified", fixed=TRUE)
 })
@@ -30,8 +17,21 @@ test_that("integer check for n", {
   expect_error(hyperslice(test.mesh.1, n="bc"), "n must be an integer", fixed=TRUE)
 })
 
-test_that("single slice works", {
-  res = hyperslice(test.mesh.1, focus.points=c(0.5, 0.5, 0.5))
-  print(res)
-  expect_identical(res, exp.slices.1)
+test_that("single slice of a single triangle", {
+  res = hyperslice(test.mesh.1, focus.points=c(0.5, 0.5, 0.8))
+  #print(res)
+  exp.slices.1 = list(
+    focusPoints = data.frame(x1=0.5, x2=0.5, x3=0.8),
+    slices = data.frame(
+      d1Min=c(0.8, 0.0, 0.0),
+      d2Min=c(0.8, 0.5, 0.5),
+      d1Max=c(0.0, 0.0, 0.5),
+      d2Max=c(0.2, 0.5, 0.5),
+      d1=c(1, 1, 2),
+      d2=c(2, 3, 3),
+      fpid=c(1, 1, 1)
+    )
+  )
+  class(exp.slices.1) = "SliceSet"
+  expect_equal(res, exp.slices.1)
 })
