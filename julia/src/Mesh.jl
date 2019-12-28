4168b14b6b7f761860bb07be8dbb0c9f77702e66
Thomas Torsney-Weir <torsneyt@gmail.com>
2019-12-28 12:44:43 +0000

    hdf5-based mesh file storage

---
+++ julia/src/Mesh.jl
@@ -0,0 +1,31 @@
+module Mesh
+
+include("types.jl")
+
+using HDF5
+
+function savehdf5(filename::String, m::Mesh)
+  dimnames = keys(m.problemSpec)
+  dimranges = values(m.problemSpec)
+
+  h5open(filename, "w") do file
+    write(file, "dimnames", dimnames)
+    write(file, "support", dimranges)
+    write(file, "points", m.points)
+    write(file, "simplices", m.simplIdx)
+  end
+end
+
+function readhdf5(filename::String)
+  h5open(filename, "r") do file
+    dimnames = read(file, "dimnames")
+    support = read(file, "support")
+    points = read(file, "points")
+    simplIdxs = read(file, "simplices")
+
+    ps = ProblemSpec(x[1] => Tuple(x[2]) for x=zip(dimnames, support))
+    Mesh(ps, points, simplIdxs)
+  end
+end
+
+end
