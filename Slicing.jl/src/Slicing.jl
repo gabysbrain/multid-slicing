72a983278642816c3fa10e84719a39c66deda205
Thomas Torsney-Weir <torsneyt@gmail.com>
2020-01-07 22:11:06 +0000

    fill in missing values in focus points

--- Slicing.jl/src/Slicing.jl
+++ Slicing.jl/src/Slicing.jl
@@ -3,9 +3,13 @@ module Slicing
 include("types.jl")
 include("Hyperslice.jl")
 
-function fillfps(ps::ProblemSpec, fp::Array{Union{Missing,Float64},1}, n::Int = 50)
+#function fillfps(ps::ProblemSpec, fp::Array{Union{Missing,Float64},1}, n::Int = 50)
+function fillfps(ps, fp, n = 50)
+  if !any(map(ismissing, fp)) # special case
+    return [fp]
+  end
   fpvals =
-    [ismissing(x[1]) ? (x[2][1] .+ (x[2][2]-x[2][1]) .* rand(n)) : repeat([x[1]], n) for x in zip(fp, ps.vals)]
+    [ismissing(x[1]) ? (x[2][1] .+ (x[2][2]-x[2][1]) .* rand(n)) : fill(x[1], n) for x in zip(fp, ps.vals)]
   # transpose everything
   [[fpvals[c][r] for c=1:length(fp)] for r = 1:n]
 end
--- Slicing.jl/test/runtests.jl
+++ Slicing.jl/test/runtests.jl
@@ -2,4 +2,5 @@
 using Test
 
 @testset "Simplex/point intersection" begin include("TestSimplexPointIntersection.jl") end
+@testset "Additional focus point sampling" begin include("TestSampling.jl") end
 @testset "Utilities" begin include("TestUtil.jl") end
