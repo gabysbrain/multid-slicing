f7b718f5fc7e1c04f9f70ecfb9fcbd35fb1f5a5e
Thomas Torsney-Weir <torsneyt@gmail.com>
2020-01-07 17:30:43 +0000

    add fillfps function

--- Slicing.jl/src/Slicing.jl
+++ Slicing.jl/src/Slicing.jl
@@ -1,5 +1,13 @@
 module Slicing
 
-import("Hyperslice.jl")
+include("types.jl")
+include("Hyperslice.jl")
+
+function fillfps(ps::ProblemSpec, fp::Array{Union{Missing,Float64},1}, n::Int = 50)
+  fpvals =
+    [ismissing(x[1]) ? (x[2][1] .+ (x[2][2]-x[2][1]) .* rand(n)) : repeat([x[1]], n) for x in zip(fp, ps.vals)]
+  # transpose everything
+  [[fpvals[c][r] for c=1:length(fp)] for r = 1:n]
+end
 
 end # module
