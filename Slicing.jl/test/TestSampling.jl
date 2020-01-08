b143b180d9cc76e1a47597c8d7b920e33649707b
Thomas Torsney-Weir <torsneyt@gmail.com>
2020-01-08 08:20:19 +0000

    Fix sampling tests

--- Slicing.jl/test/TestSampling.jl
+++ Slicing.jl/test/TestSampling.jl
@@ -1,20 +1,21 @@
 using Test
 
-include("../src/Slicing.jl")
 include("../src/types.jl")
+
+using Slicing
 
 @testset "random values are within problem spec range" begin
   ps = ProblemSpec("x1" => (-2., 2.), "x2" => (0., 1.), "x3" => (25., 50.))
 
-  fps = fillfps(ps, [missing, 0., 0.], 500)
+  fps = Slicing.fillfps(ps, [missing, 0., 0.], 500)
   @test all([x[1] >= ps.vals[1][1] for x in fps])
   @test all([x[1] <= ps.vals[1][2] for x in fps])
 
-  fps = fillfps(ps, [0., missing, 0.], 500)
+  fps = Slicing.fillfps(ps, [0., missing, 0.], 500)
   @test all([x[2] >= ps.vals[2][1] for x in fps])
   @test all([x[2] <= ps.vals[2][2] for x in fps])
 
-  fps = fillfps(ps, [0., 0., missing], 500)
+  fps = Slicing.fillfps(ps, [0., 0., missing], 500)
   @test all([x[3] >= ps.vals[3][1] for x in fps])
   @test all([x[3] <= ps.vals[3][2] for x in fps])
 end
@@ -22,6 +23,6 @@ end
 @testset "properly handles fully-specified fp" begin
   ps = ProblemSpec("x1" => (-2., 2.), "x2" => (0., 1.), "x3" => (25., 50.))
 
-  fps = fillfps(ps, [0., 0., 0.], 50)
+  fps = Slicing.fillfps(ps, [0., 0., 0.], 50)
   @test length(fps) == 1
 end
