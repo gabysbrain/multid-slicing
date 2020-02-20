2a209b679ce9a0bbf22a28bc0789e15cb3fe4d3b
Thomas Torsney-Weir <torsneyt@gmail.com>
2020-02-20 10:27:11 +0000

    add required approx function arguments

--- Slicing.jl/src/types.jl
+++ Slicing.jl/src/types.jl
@@ -15,12 +15,12 @@ struct Intersect2D
   p2d2 :: Float64
 end
 
-function Base.isapprox(x::Intersect2D, y::Intersect2D) #; atol::Real=0, rtol::Real=rtoldefault(x,y,atol), nans::Bool=false)
+function Base.isapprox(x::Intersect2D, y::Intersect2D; atol::Real=0, rtol::Real=rtoldefault(x,y,atol), nans::Bool=false)
   # FIXME: easy to break
-  isapprox(x.p1d1, y.p1d1) &&
-  isapprox(x.p1d2, y.p1d2) &&
-  isapprox(x.p2d1, y.p2d1) &&
-  isapprox(x.p2d2, y.p2d2)
+  isapprox(x.p1d1, y.p1d1, atol, rtol, nans) &&
+  isapprox(x.p1d2, y.p1d2, atol, rtol, nans) &&
+  isapprox(x.p2d1, y.p2d1, atol, rtol, nans) &&
+  isapprox(x.p2d2, y.p2d2, atol, rtol, nans)
 end
 
 PointND = Array{Float64,1}
@@ -29,4 +29,3 @@ LambdaND = Array{Float64,1}
 Simplex = Array{Float64}
 
 ProblemSpec = OrderedDict{String,Tuple{Float64,Float64}}
-
