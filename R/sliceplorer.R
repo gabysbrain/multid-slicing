169b668710e0d0b6e201793e7f126f0338bb48ee
Thomas Torsney-Weir <torsneyt@gmail.com>
2020-03-13 10:34:14 +0000

    add documentation for sliceplorer

--- R/sliceplorer.R
+++ R/sliceplorer.R
@@ -1,6 +1,20 @@
 
 #' Create a number of 1D slices of a function
 #'
+#' @param f A function that takes a single vector parameter and returns a single scalar value
+#' @param problemSpec A \code{ProblemSpec} object containing the limits of parameters
+#' @param n The number of slices to create
+#' @param density (optional) The resolution along each axis to create the plots
+#' @return An object of class \code{SliceSet1D}:
+#'           \code{problemSpec} a reference to the problem spec passed into the function
+#'           \code{focusPoints} a table containing a list of focus points of \code{n} rows
+#'           \code{slices} a table of slice segments (line segments) consisting of the following columns:
+#'                         \begin{enumerate}
+#'                           \item \code{fpid} focus point id (index into \code{focusPoints})
+#'                           \item \code{d} dimension number of the slice
+#'                           \item \code{x} x-coordinate of the slice segment
+#'                           \item \code{y} y-coordinate of the slice segment
+#'                         \end{enumerate}
 sliceplorer = function(f, problemSpec, n, density=25) {
   d = length(problemSpec$dimNames)
   focus.points = sample.ProblemSpec(problemSpec, n)
